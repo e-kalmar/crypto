bfaaa6fad9bed1566b9a3987e69acf786dc6d916
aveKOKO420 <kaloyan.st.ivanov@gmail.com>
2021-11-18 11:08:04 +0200

    Coin page fix

--- resources/views/admin/crypto/show.blade.php
+++ resources/views/admin/crypto/show.blade.php
@@ -8,18 +8,20 @@
 
                 </div>
                 <div class="col-10">
-                    <div class="card card-coin">
-                        <div class="container-fluid card-name">
-                            <h1 class="text-black-50">{{$crypto->name}}</h1>
-                        </div>
+                    <div class="card">
+
                         <div class="card-header ">
+                            
+                            <h1 class="text-black-50">{{$crypto->name}}</h1>
                             <h3 class="card-title">Current price: {{round($crypto->price)}}</h3>
-                            <div class="image ">
-                                <img src="{{$crypto->image}}" class="img-fluid rounded "
-                                     alt="{{$crypto->name}} image">
-                            </div>
+                            
+                               
+                            
                         </div>
                         <div class="card-body">
+                            <img src="{{$crypto->image}}" class="img-fluid rounded  mx-auto d-block"
+                            alt="{{$crypto->name}} image">
+                            <hr>
                             <div class="text-center">{!!$description!!}</div>
                         </div>
                     </div>
