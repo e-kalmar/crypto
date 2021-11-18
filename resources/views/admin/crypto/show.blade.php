60b3b38f173c6c4e72edecc7c3a10b954b846241
aveKOKO420 <kaloyan.st.ivanov@gmail.com>
2021-11-18 10:46:36 +0200

    fix show blade

--- resources/views/admin/crypto/show.blade.php
+++ resources/views/admin/crypto/show.blade.php
@@ -1,25 +1,35 @@
-<x-admin-master>
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.1/css/jquery.dataTables.css">
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+@extends('layouts.app')
 
-    @section('content')
-        <div class="col-6">
-            <div class="card card-coin">
-                <div class="container-fluid card-name">
-                    <h1 class="text-black-50">{{$crypto->name}}</h1>
-                </div>                
-                <div class="card-header">
-                    <h3 class="card-title">Current price: {{round($crypto->price)}}</h3>  
-                    <div class="image">
-                      <img src="{{$crypto->image}}" class="rounded mx-auto d-block p-5" alt="{{$crypto->name}} image" >
+@section('content')
+    <section class="content">
+        <div class="container-fluid pt-3">
+            <div class="row d-flex">
+                <div class="col">
+
+                </div>
+                <div class="col-10">
+                    <div class="card card-coin">
+                        <div class="container-fluid card-name">
+                            <h1 class="text-black-50">{{$crypto->name}}</h1>
+                        </div>
+                        <div class="card-header ">
+                            <h3 class="card-title">Current price: {{round($crypto->price)}}</h3>
+                            <div class="image ">
+                                <img src="{{$crypto->image}}" class="img-fluid rounded "
+                                     alt="{{$crypto->name}} image">
+                            </div>
+                        </div>
+                        <div class="card-body">
+                            <div class="text-center">{!!$description!!}</div>
+                        </div>
                     </div>
                 </div>
-                <div class="card-body">
-                    <div class="text-center">{!!$description!!}</div>
+                <div class="col">
+
                 </div>
             </div>
         </div>
+    </section>
 
-    @endsection
+@endsection
 
-</x-admin-master>
